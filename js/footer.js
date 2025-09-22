70f59823fdfffb4aa111a220d5a99ab250d1d861
EtienneLm <112947657+EtienneLm@users.noreply.github.com>
2025-09-22 13:49:15 +0200

    Delete js directory

--- js/footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-fetch("/GetYourTabs/pages/accessories/footer.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("footer-placeholder").innerHTML = data;
-      document.dispatchEvent(new Event("footerLoaded"));
-    })
-    .catch(error => console.error("Error loading footer:", error));
-});
