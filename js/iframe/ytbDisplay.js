c08d165a9b74af128ddb7cf8393cc45f9bec434c
EtienneLm <112947657+EtienneLm@users.noreply.github.com>
2025-09-22 13:57:22 +0200

    Update ytbDisplay.js

--- js/iframe/ytbDisplay.js
+++ js/iframe/ytbDisplay.js
@@ -20,10 +20,10 @@ export function changeYtbDisplay() {
 
   const iconImg = changeYtbDisplayBtn.querySelector('img');
   if (ytbVisible) {
-    iconImg.src = "../assets/icons/ytb-logo-2.svg";
+    iconImg.src = "/GetYourTabs/assets/icons/ytb-logo-2.svg";
     iconImg.alt = "shown ytb logo";
   } else {
-    iconImg.src = "../assets/icons/ytb-logo-1.svg";
+    iconImg.src = "/GetYourTabs/assets/icons/ytb-logo-1.svg";
     iconImg.alt = "hidden ytb logo";
   }
 }
